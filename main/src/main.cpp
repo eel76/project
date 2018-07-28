04cd13ec89fb14fa77662f42e6958748fdbd2a37
Dirk Reinbach <dirk.reinbach@gmail.com>
2018-07-28 21:58:46 +0200

    work in progress

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,52 +1,46 @@
 #include "reducer.h"
 #include "store.h"
 
+#include <iostream>
 #include <tuple>
 #include <vector>
 
-enum class VisibilityFilter
-{
-  SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE
-};
+enum class VisibilityFilter { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE };
 
 struct Todo
 {
   std::string text;
-  bool completed;
+  bool        completed;
 };
 
 using Todos = std::vector<Todo>;
-using Model = std::tuple <VisibilityFilter, Todos>;
+using Model = std::tuple<VisibilityFilter, Todos>;
 
 struct AddTodo
 {
   std::string text;
 
-  Todos operator() (Todos todos) const
-  {
+  Todos operator()(Todos todos) const {
     todos.push_back(Todo{ text, false });
     return todos;
   }
 };
 
-auto addTodo(std::string text)
-{
-  return AddTodo{text};
+auto addTodo(std::string text) {
+  return AddTodo{ text };
 }
 
 struct ToggleTodo
 {
   int index;
 
-  Todos operator() (Todos todos) const
-  {
+  Todos operator()(Todos todos) const {
     todos[index].completed = !todos[index].completed;
     return todos;
   }
 };
 
-auto toggleTodo(int index)
-{
+auto toggleTodo(int index) {
   return ToggleTodo{ index };
 }
 
@@ -54,31 +48,26 @@ struct SetVisibilityFilter
 {
   VisibilityFilter visibilityFilter;
 
-  VisibilityFilter operator() (VisibilityFilter) const
-  {
+  VisibilityFilter operator()(VisibilityFilter) const {
     return visibilityFilter;
   }
 };
 
-auto setVisibilityFilter(VisibilityFilter visibilityFilter)
-{
+auto setVisibilityFilter(VisibilityFilter visibilityFilter) {
   return SetVisibilityFilter{ visibilityFilter };
 }
 
-auto draw()
-{
-  return [](auto) {};
+auto draw() {
+  return [](auto) { std::cout << "draw\n"; };
 }
 
-
-int main ()
-{
+int main() {
   auto const visibilityFilter = makeReducer<SetVisibilityFilter>();
-  auto const todos = makeReducer<AddTodo, ToggleTodo>();
+  auto const todos            = makeReducer<AddTodo, ToggleTodo>();
 
   auto const todoApp = combineReducers(visibilityFilter, todos);
 
-  auto store = Store{ Model{}, todoApp, draw () };
+  auto store = Store{ Model{}, todoApp, draw() };
 
   store.dispatch(addTodo("Learn about actions"));
   store.dispatch(addTodo("Learn about reducers"));
@@ -87,8 +76,8 @@ int main ()
   store.dispatch(toggleTodo(1));
   store.dispatch(setVisibilityFilter(VisibilityFilter::SHOW_COMPLETED));
 
-  //auto event = char{};
+  // auto event = char{};
 
-  //while (std::cin >> event)
+  // while (std::cin >> event)
   //  dispatch (store, event);
 }
